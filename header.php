a50752ab31c23590624a19013815b0f354ce0eb2
RGonzalez578 <ronald.chavesgonzalez@ucr.ac.cr>
2022-04-26 16:31:03 -0600

    Implementación navbar desktop

--- header.php
+++ header.php
@@ -19,13 +19,23 @@
                 <div class="col-2">
                     <p class="logo">Find.me</p>
                 </div>
-                <div class="col-8">
-                    <input class="search-bar" type="text" placeholder="Search something...">
-                    <a class="employee-btn" href="#"><img src="./assets/employee.svg" alt=""></a>
+                <div class="col-8 d-flex">
+
+                    <div class="input-group m-auto">
+                        <input type="text" class="form-control search-bar" placeholder="Search something..." aria-label="Search something..." aria-describedby="button-submit">
+                        <button class="btn search-btn" type="button" id="button-submit"><img src="<?php echo get_theme_file_uri( 'assets/search-icon.svg' ); ?>" alt="search-button"></button>
+                    </div>
+
+                    <div class="container-btn-employee">
+                        <a class="employee-btn" href="#"><img src=" <?php echo get_theme_file_uri( 'assets/employee.svg' ); ?>" alt="employee button"></a>
+                    </div>
+
                 </div>
+
                 <div class="col-2 d-flex justify-content-end">
-                    <a class="user-btn" href="#">Username <img src="./assets/user-icon.svg" alt=""></a>
+                    <a class="user-btn" href="#">Username <img src=" <?php echo get_theme_file_uri( 'assets/user-icon.svg' ); ?> " alt="user button"></a>
                 </div>
+
             </nav>
         </header>
         <!-- header -->
