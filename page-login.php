b41a12b1fd06fed6abb77082c590e873ba074de1
WLARA230700 <willy.lara@ucr.ac.cr>
2022-04-26 16:24:39 -0600

    Login responsive

--- page-login.php
+++ page-login.php
@@ -4,12 +4,12 @@
 
    <div class="content-fluid">
        <div class="row purple-bg">
-           <div class="col-sm-6 heigh100 text-center">
+           <div class="col-sm-6 heigh100-l text-center">
                 <h1 class="findme-title">Find.me</h1>
                 <p class="are-new">Are you new in this app?</p>
                 <a class="signup-btn" href="#">SIGN UP</a>
            </div>
-           <div class="col-sm-6 heigh100">
+           <div class="col-sm-6 heigh100-r">
                 <div class="card">
                     <h2 class="subtitles">LOG IN</h2>
                     <form class="form-login" action="#">
