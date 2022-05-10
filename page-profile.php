78ece6749899795e29ed8338ed953b6c3c641a3c
Abby-Lopez <70771771+Abby-Lopez@users.noreply.github.com>
2022-05-10 13:13:41 -0600

    Profile

---
+++ page-profile.php
@@ -0,0 +1,116 @@
+<?php get_header("header"); ?>
+
+    <!-- content -->
+
+   <div class="content gray-bg">
+       <div class="row mg-top-4">
+           <!-- col-8 -->
+           
+           <div class="col-8">
+               <div class="row">
+                   <div class="col">
+                        <div class="profile-card">
+                            <h1>User Name</h1> 
+                            <img src="" alt="">
+                            <img src="" alt="">
+                            <h3>City, Country</h3>
+                            <h3>English, Spanish</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <button>+506 80347004</button>
+                            <button>user@gmail.com</button>
+                            <button>www.user.com</button>
+                        </div> 
+                   </div>
+               </div> 
+
+               <div class="row">
+                    <div class="col">
+                        <div class="portfolio-card">
+                        <h2 class="profile-subtitles">Portafolio</h2>
+                        <div class="row">
+                        <div class="col">
+                                <div class="thumbnail">
+                                     <a href="#">
+                                        <img src="https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-sin-imagen-en-miniatura-marcador-de-posici%C3%B3n-para-foros-blogs-y-sitios-web.jpg?ver=6" alt="" style="width:80%">
+                                       
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="thumbnail">
+                                     <a href="#">
+                                        <img src="https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-sin-imagen-en-miniatura-marcador-de-posici%C3%B3n-para-foros-blogs-y-sitios-web.jpg?ver=6" alt="" style="width:80%">
+                                       
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="thumbnail">
+                                     <a href="#">
+                                        <img src="https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-sin-imagen-en-miniatura-marcador-de-posici%C3%B3n-para-foros-blogs-y-sitios-web.jpg?ver=6" alt="" style="width:80%">
+                                       
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                            
+
+                            
+                        </div> 
+                   </div> 
+               </div> 
+
+           </div>
+        
+           <!-- col-4 -->
+           <div class="col-4 ">
+               <div class="row">
+                    <div class="col mg-top-20">
+                        <div class="  skills-card">
+                            <h2 class="profile-subtitles">Skills</h2> 
+                            <p class="skills-subtitles">Name of the skill</p>  
+                            <p class="skills-subtitles">Name of the skill</p>  
+                        </div> 
+                    </div>  
+               </div> 
+
+               <div class="row">
+                    <div class="col">
+                        <div class="more-skills-card">
+                            <h2 class="profile-subtitles">Experience</h2> 
+                            <p class="skills-subtitles">Experience</p>  
+                            <p class="skills-subtitles">Experience</p>    
+                        </div> 
+                    </div> 
+               </div> 
+
+               <div class="row">
+                    <div class="col">
+                        <div class="more-skills-card">
+                            <h2 class="profile-subtitles">Education</h2>  
+                            <p class="skills-subtitles">Education</p>  
+                            <p class="skills-subtitles">Education</p>   
+                        </div> 
+                    </div> 
+               </div> 
+
+               <div class="row">
+                    <div class="col">
+                        <div class="more-skills-card">
+                            <h2 class="profile-subtitles">Soft Skills</h2>   
+                            <p class="skills-subtitles">Skill</p>  
+                            <p class="skills-subtitles">Skill</p>  
+                        </div> 
+                    </div> 
+               </div> 
+
+           </div>
+
+          
+       </div>
+   </div>
+
+    <!-- content -->  
+
+<?php get_footer("footer"); ?>
