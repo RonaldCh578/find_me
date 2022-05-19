bc500843c807ff45890e5c914720355ad558dea1
RGonzalez578 <ronald.chavesgonzalez@ucr.ac.cr>
2022-05-18 21:11:20 -0600

    Replanteo de página de inicio - desktop

--- functions.php
+++ functions.php
@@ -6,14 +6,18 @@
     //Bootstrap_JS
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
 
-    //Cargar el tema CSS
     function load_assets(){
+        //Cargar el tema CSS
         wp_enqueue_style('style', get_stylesheet_uri());
+
+        //Cargar main JS
+        wp_enqueue_script( 'main_js', get_template_directory_uri() . '/assets/js/main.js');
     }
 
     add_action('wp_enqueue_scripts', 'load_assets');
 
     //Soporte para imagenes destacadas en páginas
     add_theme_support('post-thumbnails');
+    add_theme_support('menus');
 
 ?>
