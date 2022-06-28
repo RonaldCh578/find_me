e32fe772f8aecc9681d4ae23590bd5c67e9003ee
WLARA230700 <willy.lara@ucr.ac.cr>
2022-06-28 13:10:31 -0600

    Includes jobs

--- includes/en/jobs.php
+++ includes/en/jobs.php
@@ -9,7 +9,32 @@
     while($wpb_all_query->have_posts()): $wpb_all_query->the_post();
 ?>
 
-<p><?php the_title(); ?></p>
+<!-- CARD USER -->
+<a href="../jobdetail" class="cardLink">
+    <div class="row card-user">
+        <div class="col-sm-3 d-flex justify-content-center align-items-center">
+            <div class="container-photo">
+                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>" alt="default image">
+            </div>
+        </div>
+        <div class="col-sm-9">
+            <div class="info-user">
+                <h4><?php the_title(); ?></h4>         
+                <b>
+                    <span>
+                        <span>Company Name</span>
+                        <span>|</span>
+                        <span>San José, SJ</span>
+                        <span>|</span>
+                        <span>Full Time</span>
+                    </span>
+                </b>
+                <p><?php the_content(); ?></p>
+            </div>
+        </div>                        
+    </div>
+</a>                
+<!-- CARD USER -->
 
 <?php
     wp_reset_postdata();
